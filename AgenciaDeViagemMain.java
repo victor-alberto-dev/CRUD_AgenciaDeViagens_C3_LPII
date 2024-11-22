678075c4d51b5f7a3f02abc2e5a294f6a8f014ac
Victor Alberto <victoralbertopassos@hotmail.com>
2024-11-22 12:03:39 -0300

    Deletei comentários desnecessários

--- AgenciaDeViagemMain.java
+++ AgenciaDeViagemMain.java
@@ -5,8 +5,6 @@ public class AgenciaDeViagemMain {
 	static StringBuffer memoriaPacotesDeViagem = new StringBuffer();
 	static StringBuffer memoriaClientes = new StringBuffer();
 	static Scanner scan = new Scanner (System.in);
-    //Mudou aqui
-    //Mudei de novo
 	public static void main(String[] args) {
 		char opcao, resp = 'N';
 		do {
